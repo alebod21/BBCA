8668d165f385f176f806eaa02e7e87c9610f7df6
Alec Bodnar <34168697+alebod21@users.noreply.github.com>
2020-10-05 19:21:14 -0400

    Created New Branch w/ changes
    - Moved serverClientHandler to own file
    - When sending a message doesn't broadcast back to yourself

--- ChatServer/src/ChatServer.java
+++ ChatServer/src/ChatServer.java
@@ -1,7 +1,6 @@
 import java.net.Inet4Address;
 import java.net.ServerSocket;
 import java.net.Socket;
-// import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -10,26 +9,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 
-
 public class ChatServer {
     public static final int PORT = 54321;
-    private static final ArrayList<ClientConnectionData> clientList = new ArrayList<>();
+    public static final ArrayList<ClientConnectionData> clientList = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
         ExecutorService pool = Executors.newFixedThreadPool(100);
 
-        try (ServerSocket serverSocket = new ServerSocket(PORT)){
+        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
             System.out.println("Chat Server started.");
             System.out.println("Local IP: "
                     + Inet4Address.getLocalHost().getHostAddress());
             System.out.println("Local Port: " + serverSocket.getLocalPort());
-        
+
             while (true) {
                 try {
                     Socket socket = serverSocket.accept();
                     System.out.printf("Connected to %s:%d on local port %d\n",
-                        socket.getInetAddress(), socket.getPort(), socket.getLocalPort());
-                    
+                            socket.getInetAddress(), socket.getPort(), socket.getLocalPort());
+
                     // This code should really be done in the separate thread should be passed to client in constructor
                     BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
                     PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
@@ -40,16 +38,16 @@ public class ChatServer {
                     synchronized (clientList) {
                         clientList.add(client);
                     }
-                    
+
                     System.out.println("added client " + name);
 
                     //handle client business in another thread
-                    pool.execute(new ClientHandler(client));
+                    pool.execute(new ServerClientHandler(client));
                 } catch (IOException ex) {
                     System.out.println(ex.getMessage());
                 }
 
             }
-        } 
+        }
     }
 }
--- ChatServer/src/ServerClientHandler.java
+++ ChatServer/src/ServerClientHandler.java
@@ -1,21 +1,23 @@
-public class ClientHandler implements Runnable{
-    
-    // Maintain data about the client serviced by this thread
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.SocketException;
+
+class ServerClientHandler implements Runnable{
     ClientConnectionData client;
 
-    public ClientHandler(ClientConnectionData client) {
+    ServerClientHandler(ClientConnectionData client) {
         this.client = client;
     }
 
-    /**
-     * Broadcasts a message to all clients connected to the server.
-     */
     public void broadcast(String msg) {
         try {
             System.out.println("Broadcasting -- " + msg);
-            synchronized (clientList) {
-                for (ClientConnectionData c : clientList){
-                    c.getOut().println(msg);
+            synchronized (ChatServer.clientList) {
+                for (ClientConnectionData c : ChatServer.clientList){
+                    if(c.equals(client) && !msg.substring(0, 7).equals("WELCOME")) { //Exception for welcome doesn't necessarily need to be here
+                    } else {
+                        c.getOut().println(msg);
+                    }
                     // c.getOut().flush();
                 }
             }
@@ -23,7 +25,7 @@ public class ClientHandler implements Runnable{
             System.out.println("broadcast caught exception: " + ex);
             ex.printStackTrace();
         }
-        
+
     }
 
     @Override
@@ -36,15 +38,15 @@ public class ClientHandler implements Runnable{
             //notify all that client has joined
             broadcast(String.format("WELCOME %s", client.getUserName()));
 
-            
+
             String incoming = "";
 
             while( (incoming = in.readLine()) != null) {
-                if (incoming.startsWith("CHAT")) {
+                if (incoming.toUpperCase().startsWith("CHAT")) {
                     String chat = incoming.substring(4).trim();
                     if (chat.length() > 0) {
                         String msg = String.format("CHAT %s %s", client.getUserName(), chat);
-                        broadcast(msg);    
+                        broadcast(msg);
                     }
                 } else if (incoming.startsWith("QUIT")){
                     break;
@@ -52,16 +54,16 @@ public class ClientHandler implements Runnable{
             }
         } catch (Exception ex) {
             if (ex instanceof SocketException) {
-                System.out.println("Caught socket ex for " + 
-                    client.getName());
+                System.out.println("Caught socket ex for " +
+                        client.getName());
             } else {
                 System.out.println(ex);
                 ex.printStackTrace();
             }
         } finally {
             //Remove client from clientList, notify all
-            synchronized (clientList) {
-                clientList.remove(client); 
+            synchronized (ChatServer.clientList) {
+                ChatServer.clientList.remove(client);
             }
             System.out.println(client.getName() + " has left.");
             broadcast(String.format("EXIT %s", client.getUserName()));
