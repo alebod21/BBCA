5612afe7af0d84dad4a3b7c8c4ed6a5cee1e1f41
Alec Bodnar <34168697+alebod21@users.noreply.github.com>
2020-10-25 19:41:42 -0400

    fixed one linw
    plus other stuff

--- ChatClient/src/ChatClientGuiRunner.java
+++ ChatClient/src/ChatClientGuiRunner.java
@@ -18,7 +18,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-// public class ChatClientGuiRunner extends Application{
+public class ChatClientGuiRunner extends Application{
 
     public static void main(String[] args) {
         launch(args);
